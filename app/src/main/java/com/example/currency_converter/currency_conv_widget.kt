02639c2207ddd71982505e02502e97d0b171ca25
sonulen <sonulen@gmail.com>
2019-09-15 20:29:10 +0300

    update: serialize response to map

--- app/src/main/java/com/example/currency_converter/currency_conv_widget.kt
+++ app/src/main/java/com/example/currency_converter/currency_conv_widget.kt
@@ -1,10 +1,13 @@
 package com.example.currency_converter
 
 import android.view.View
+import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
-
+import android.widget.ArrayAdapter
+import android.R
+import android.widget.Spinner
 
 
 enum class Master {
@@ -18,11 +21,9 @@ enum class Master {
 
 class Currency_converter_widget(instance:MainActivity) {
     var instance:MainActivity
-    var count_from:Double = 0.0
+    var count_from:Double? = 0.0
     var count_to: Double = 0.0
-    var currency_from: String = "USD"
-    var currency_to: String = "RUB"
-    var coeff: Double = 0.0
+    var rates: Map<String, Double>? = null
 
     private val client = OkHttpClient()
 
@@ -37,7 +38,11 @@ class Currency_converter_widget(instance:MainActivity) {
             .build()
 
         client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {}
+            override fun onFailure(call: Call, e: IOException) {
+                // Попробуем еще раз через 1 сек.
+                Thread.sleep(1000)
+                run("http://data.fixer.io/api/latest?access_key=4353a124be0c1f20ccc43039c18c3d38")
+            }
             override fun onResponse(call: Call, response: Response) {
                 success_load(response)
             }
@@ -46,20 +51,34 @@ class Currency_converter_widget(instance:MainActivity) {
 
     fun update_convert_from_count(new_count:Double) {
         count_from = new_count
-        count_to = new_count * coeff
+        count_to = new_count * 0
     }
 
     fun update_convert_to_count(new_count:Double) {
         count_to = new_count
-        count_from = new_count / coeff
+        count_from = new_count / 0
     }
 
     fun success_load(response: Response) {
+        val gson = Gson()
+        // response.body может быть использован только один раз
+        rates = gson.fromJson(response.body?.string(), ResponseFixerIo::class.java).rates
+
         instance.runOnUiThread(Runnable {
             instance.layout_with_progress_bar.visibility = View.INVISIBLE
             instance.layout_main.visibility = View.VISIBLE
+
+
+            // Create an ArrayAdapter using the string array and a default spinner layout
+            val adapter = ArrayAdapter<String>(
+                instance, android.R.layout.simple_spinner_item, ArrayList(rates!!.keys)
+            )
+
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            instance.spinner3.adapter = adapter
+            instance.spinner4.adapter = adapter
         })
-        println(response.body?.string())
+
     }
 
 
---
+++ app/src/main/java/com/example/currency_converter/serialize.kt
@@ -0,0 +1,10 @@
+package com.example.currency_converter
+
+class ResponseFixerIo {
+    internal var success: String? = null
+    internal var timestamp: Int? = null
+    internal var base: String? = null
+    internal var date: String? = null
+    internal val rates: Map<String, Double>? = null
+}
+
